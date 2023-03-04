1289e7b83e9f879cc36f4f914478f0b0a59fbf2b
mohammed-gharbi <63358440+mohammed-gharbi@users.noreply.github.com>
2023-03-04 22:16:01 +0100

    solid intro

---
+++ main.rb
@@ -0,0 +1,14 @@
+# SOLID DESIGN PRINCIPLES
+
+# solid design principles help you write code that is easy to reuse and and extend.
+
+# solid was mentioned first by Robert Martin (Uncle Bob) in 2000
+
+# solid stands for 5 principles:
+## Single Responsability
+## Open/closed
+## Liskov Substitution
+## Interface segregation
+## Sependency inversion
+
+
